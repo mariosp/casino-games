8da7dd0a7a85bd866f992246191617f0e04c2a73
Marios Patsis <mariospatsis@gmail.com>
2020-03-22 17:00:29 +0200

    img error handle

--- src/components/Games/Game/Game.js
+++ src/components/Games/Game/Game.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Game.css";
 import CaptionBox from "../CaptionBox/CaptionBox";
 
 const Game = ({game}) => {
+    const [imgError, setImgError] = useState(false);
 
+    const onError = () => setImgError(true);
     return(
         <div className="card-wrapper" >
-            <img src={game.image} className="card-image" alt={game.name} />
+            {!imgError? <img src={game.image} className="card-image" alt={game.name} onError={onError} />
+                : <div className="card-image-fallback"></div>
+            }
             <CaptionBox caption={game.name} top/>
-            <div className="image-caption">
+                <div className="image-caption">
                 <div className="icon-box">
                     <div className="background-box"></div>
                     <i className="fa fa-play icon-style"></i>
